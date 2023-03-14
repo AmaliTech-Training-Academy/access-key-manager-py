2286d0d6e8997cc5ab30776312c78905ad7ac313
Millicent-Konadu <millicent.opoku@amalitech.com>
2023-03-14 16:17:40 +0000

    User authenetication and schoolapp done

--- access_key_manager/access_key_manager/settings.py
+++ access_key_manager/access_key_manager/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'schoolapp',
+    'authentication',
 ]
+AUTH_USER_MODEL = "authentication.CustomUser"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
---
+++ access_key_manager/schoolapp/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+# from .models import AccessKey
+
+# def access_key_list(request):
+#     access_keys = AccessKey.objects.all()
+#     return render(request, 'access_key_list.html', {'access_keys': access_keys})
+
