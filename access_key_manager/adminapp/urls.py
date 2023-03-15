df23a3b19fbc8cebda5a61794a4fca8315497c9f
d.danso <douglas.danso@amalitech.com>
2023-03-15 14:16:39 +0000

    admin

--- access_key_manager/adminapp/urls.py
+++ access_key_manager/adminapp/urls.py
@@ -5,4 +5,6 @@ from .views import AccessKeyListView
 urlpatterns = [
     path('feed/', AccessKeyListView.as_view(), name='access_key_list'),
     path('generate/', views.access_key_generate, name='access_key_generate'),
+    path('update/<int:access_key_id>/', views.access_key_update, name='access_key_update'),
+    path('access_key/<int:access_key_id>/revoke/', views.revoke_key, name='access_key_revoke'),
 ]
