d3568e8ccc650d496d9115333e7687e500c10248
Millicent-Konadu <millicent.opoku@amalitech.com>
2023-03-28 08:06:53 +0000

    schoolapp done

--- access_key_manager/schoolapp/models.py
+++ access_key_manager/schoolapp/models.py
@@ -7,19 +7,9 @@ class School(models.Model):
     name = models.CharField(max_length=100)
     active_key = models.ForeignKey(AccessKey, null=True, blank=True, on_delete=models.SET_NULL, related_name='school')
     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
-    # def __str__(self):
-    #     return self.name
-
-    #status, date of procurement and expiry date
-    def school_key_info(self, school):
-        if self.school == school:
-            return{
-                'status': self.status,
-                'date_of_procurement': self.date_of_procurement,
-                'expiry_date': self.expiry_date,
-            }       
-        else:
-            return None                   
+    def __str__(self):
+        return self.school.name
+              
 
 
 
